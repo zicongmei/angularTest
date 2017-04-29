4154042cdf656632c83703d88b5ce37eff1c4b60
zicongmei <zicongmei@gmail.com>
2017-04-29 08:51:13 -0700

    communite to backend by API

--- backEnd/server/server.go
+++ backEnd/server/server.go
@@ -6,6 +6,11 @@ import (
 	"path/filepath"
 )
 
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("received: " + r.URL.Path))
+}
+
 func Start(Configs loadConfig.ConfigStruct) {
 	frontendPAth, err := filepath.Abs(Configs.Server.FrontendPath)
 	if err != nil {
@@ -13,5 +18,6 @@ func Start(Configs loadConfig.ConfigStruct) {
 	}
 	frontendPAth += "/"
 	http.Handle("/", http.FileServer(http.Dir(frontendPAth)))
+	http.HandleFunc("/request/", requestHandler)
 	http.ListenAndServe(":"+Configs.Server.HttpPort, nil)
 }
--- frontEnd/experimrntCtrl.js
+++ frontEnd/experimrntCtrl.js
@@ -1,18 +1,25 @@
-app.controller('experimrntCtrl', function($scope, $location) {
+app.controller('experimrntCtrl', function($scope, $location, $http) {
     $scope.click = [];
     $scope.firstPlayer = false;
     $scope.mouseLocation = function(myE) {
         $scope.x = myE.clientX;
         $scope.y = myE.clientY;
     }
-    $scope.mouseClick = function(i, j) {
+    $scope.mouseClick = function(row, col) {
         var loc = {
-            x: i,
-            y: j,
+            x: row,
+            y: col,
             firstPlayer: $scope.firstPlayer
         };
         $scope.click.push(loc)
         $scope.firstPlayer = !$scope.firstPlayer
-        console.log($location)
+        $http({
+            method: "PUT",
+            url: "request/move?row=" + row + "&col=" + col
+        }).then(function mySucces(response) {
+            $scope.myWelcome = response.data;
+        }, function myError(response) {
+            $scope.myWelcome = response.statusText;
+        });
     }
 });
