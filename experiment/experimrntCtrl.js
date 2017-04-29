929f74d000e5370520fcd25f204adacf862ba0fc
zicongmei <zicongmei@gmail.com>
2017-04-28 20:38:47 -0700

    initial: board click

---
+++ experiment/experimrntCtrl.js
@@ -0,0 +1,13 @@
+app.controller('experimrntCtrl', function($scope) {
+    $scope.click = [];
+    $scope.firstPlayer = false;
+    $scope.mouseLocation = function(myE) {
+        $scope.x = myE.clientX;
+        $scope.y = myE.clientY;
+    }
+    $scope.mouseClick = function(i, j) {
+        var loc = {x:i, y:j, firstPlayer:$scope.firstPlayer};
+        $scope.click.push(loc)
+        $scope.firstPlayer = !$scope.firstPlayer
+    }
+});
---
+++ experiment/includes.js
@@ -0,0 +1,3 @@
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+<script src="experiment.js"></script>
+<script src="experimrntCtrl.js"></script>
