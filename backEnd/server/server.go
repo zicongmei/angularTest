0a5cac1275f0edaed82d91b4ec84dc181f5c84ae
zicongmei <zicongmei@gmail.com>
2017-04-29 19:47:37 -0700

    post login to backend

--- backEnd/server/server.go
+++ backEnd/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"github.com/zicongmei/angularTest/backEnd/loadConfig"
 	"net/http"
 	"path/filepath"
@@ -11,6 +12,19 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("received: " + r.URL.Path))
 }
 
+type UserRequest struct {
+	User     string `json:"user"`
+	Password string `json:"pwd"`
+}
+
+func authenticateHandler(w http.ResponseWriter, r *http.Request) {
+	var ur UserRequest
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&ur)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("tried to login with " + ur.User))
+}
+
 func Start(Configs loadConfig.ConfigStruct) {
 	frontendPAth, err := filepath.Abs(Configs.Server.FrontendPath)
 	if err != nil {
@@ -19,5 +33,6 @@ func Start(Configs loadConfig.ConfigStruct) {
 	frontendPAth += "/"
 	http.Handle("/", http.FileServer(http.Dir(frontendPAth)))
 	http.HandleFunc("/request/", requestHandler)
+	http.HandleFunc("/authenticate/", authenticateHandler)
 	http.ListenAndServe(":"+Configs.Server.HttpPort, nil)
 }
--- frontEnd/loginCtrl.js
+++ frontEnd/loginCtrl.js
@@ -3,5 +3,16 @@ app.controller('loginCtrl', function($scope, $http) {
     $scope.guestLogin = function(name) {
         console.log("login:" + name)
         $scope.user = name;
+        $http({
+            method: "POST",
+            url: "authenticate/login",
+            data: {
+                "user": name
+            },
+        }).then(function mySucces(response) {
+            console.log(response)
+        }, function myError(response) {
+            console.log("Failed to login $http.$$url")
+        });
     }
 });
